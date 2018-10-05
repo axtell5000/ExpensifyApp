a5c16f2e94fd3fee8f5b0dd54a71cdfa33a68da0
Stephen Axtell <axtell.stephen@gmail.com>
2018-10-05 11:47:50 +0200

    Changed the moment package as well some style changes and test

--- src/app.js
+++ src/app.js
@@ -1,34 +1,23 @@
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-
-import 'raf/polyfill';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { login, logout } from "./actions/auth";
-
-import 'normalize.css/normalize.css' //fetching from the node_module
-
-import 'react-dates/lib/css/_datepicker.css';
+import { login, logout } from './actions/auth';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {firebase} from './firebase/firebase';
+import 'react-dates/lib/css/_datepicker.css';
+import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
-
-console.log('test');
-// Provider - Must include the name of your store
 const jsx = (
   <Provider store={store}>
-    <AppRouter/>
+    <AppRouter />
   </Provider>
 );
-
 let hasRendered = false;
 const renderApp = () => {
   if (!hasRendered) {
@@ -39,9 +28,8 @@ const renderApp = () => {
 
 ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
-
 firebase.auth().onAuthStateChanged((user) => {
-  if(user) {
+  if (user) {
     store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
@@ -55,4 +43,3 @@ firebase.auth().onAuthStateChanged((user) => {
     history.push('/');
   }
 });
-
