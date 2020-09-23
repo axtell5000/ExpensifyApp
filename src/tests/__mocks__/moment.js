8bbef47a190bfb19f976e1dc3368b7063f7b4a2f
Stephen Axtell <axtell.stephen@gmail.com>
2020-09-23 10:27:39 +0200

    fixing for vulnerability reports

--- src/tests/__mocks__/moment.js
+++
@@ -1,8 +0,0 @@
-
-// In __mocks__ we are creating mock modules to help testing. Has to be __mocks__ directory name
-
-const moment = require.requireActual('moment'); // We have to reference the original not the mocked moment
-
-export default (timestamp = 0) => {
-  return moment(timestamp);
-}
